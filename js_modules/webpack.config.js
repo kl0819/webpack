3b09b5a6450b4534c01ccde2d81e1ba236ee962d
Kelly T Le <kellytnle@gmail.com>
2017-05-14 01:46:14 -0700

    added local imgs, rule img size

--- js_modules/webpack.config.js
+++ js_modules/webpack.config.js
@@ -19,6 +19,16 @@ const config = {
           use: 'css-loader'
         }),
         test: /\.css$/
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: { limit: 40000 }
+          },
+          'image-webpack-loader'
+        ]
       }
     ]
   },
