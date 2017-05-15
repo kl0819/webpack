3da5bfcddf71078605a5c97bfecc8f8e1f93dc3d
Kelly T Le <kellytnle@gmail.com>
2017-05-14 21:39:21 -0700

    code spliting

--- js_modules/src/image_viewer.js
+++ js_modules/src/image_viewer.js
@@ -1,13 +1,15 @@
-import big from '../assets/big.jpeg';
+// import big from '../assets/big.jpeg';
 import small from '../assets/small.jpeg';
 import '../styles/image_viewer.css';
 
-const image = document.createElement('img');
-image.src = small;
+export default () => {
+  const image = document.createElement('img');
+  image.src = small;
 
-document.body.appendChild(image);
+  document.body.appendChild(image);
+};
 
-const bigImage = document.createElement('img');
-bigImage.src = big;
-
-document.body.appendChild(bigImage);
+// const bigImage = document.createElement('img');
+// bigImage.src = big;
+//
+// document.body.appendChild(bigImage);
