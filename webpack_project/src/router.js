8ae9e6f8971ad8545231cf0f6cfc3d8e3ffca761
Kelly T Le <kellytnle@gmail.com>
2017-05-15 23:07:13 -0700

    Refactor: removed JSX, replaced with obj

--- webpack_project/src/router.js
+++ webpack_project/src/router.js
@@ -3,9 +3,6 @@ import { Router, Route, IndexRoute, hashHistory } from 'react-router';
 
 import Home from './components/Home';
 import ArtistMain from './components/artists/ArtistMain';
-import ArtistDetail from './components/artists/ArtistDetail';
-import ArtistCreate from './components/artists/ArtistCreate';
-import ArtistEdit from './components/artists/ArtistEdit';
 
 // Plain JS Object (aka behind the scenes jsx)
 const componentRoutes = {
@@ -40,14 +37,7 @@ const componentRoutes = {
 // JSX
 const Routes = () => {
   return (
-    <Router history={hashHistory}>
-      <Route path="/" component={Home}>
-        <IndexRoute component={ArtistMain} />
-        <Route path="artists/new" component={ArtistCreate} />
-        <Route path="artists/:id" component={ArtistDetail} />
-        <Route path="artists/:id/edit" component={ArtistEdit} />
-      </Route>
-    </Router>
+    <Router history={hashHistory} routes={componentRoutes} />
   );
 };
 
